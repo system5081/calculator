071846e07d7c07c034273241333c8ecb6642fe1e
system5081 <t182040@edu.kaiyodai.ac.jp>
2021-07-10 07:12:38 +0900

    7/10

---
+++ database/seeds/EquationsTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class EquationsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $param=[
+            "users_id"=>1,
+            "equation"=>"1+1",
+            
+            ];
+        DB::table("equations")->insert($param);
+    }
+}
